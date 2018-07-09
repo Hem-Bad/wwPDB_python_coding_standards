fe80a6bf14d09894eaa2983351b9805086dda005
Jon Wedell <wedell@bmrb.wisc.edu>
2018-07-09 11:55:27 -0500

    Add fizzbuzz example module for future unittesting demonstration.

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from typing import Union
+
+
+def fizzbuzz(number: int) -> Union[str, int]:
+    """ The classic FizzBuzz method.
+
+    Returns FizzBuzz, Fizz, Buzz, or the input number for numbers greater than zero.
+    Raises ValueError for numbers less than 10."""
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number < 0:
+        raise ValueError("Cannot FizzBuzz numbers less than 0.")
