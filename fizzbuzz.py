839cf937a148bbf2f0df8f35cefd23b2780bc314
Jon Wedell <wedell@bmrb.wisc.edu>
2018-07-12 08:54:05 -0500

    Fix typo in FizzBuzz function docstring.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,7 +7,7 @@ def fizzbuzz(number: int) -> Union[str, int]:
     """ The classic FizzBuzz method.
 
     Returns FizzBuzz, Fizz, Buzz, or the input number for numbers greater than zero.
-    Raises ValueError for numbers less than 10."""
+    Raises ValueError for numbers less than 0."""
     if number % 3 == 0 and number % 5 == 0:
         return "FizzBuzz"
     elif number % 3 == 0:
